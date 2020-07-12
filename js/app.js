59726c19af05918d670f8db61b4ab64d98ff5062
Nikita Belevich <belevish_1998@mail.ru>
2020-07-12 23:02:05 +0300

    feat: Сделано добавление класса стилей активной клавише
    Каждой нажатой клавише добавляется класс активности, CapsLock обрабатывается по особому, при отпускании клавиш класс удаляется.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 const keyboard = document.querySelector('.keyboard');
 const keyboardRows = Array.from(keyboard.children);
+const inputField = document.querySelector('.input-keyboard');
 
 addCodeAttribute();
 // Функция добавляет всем клавишам клавиатуры атрибут с соответствующим ей физическим кодом
@@ -12,7 +13,7 @@ function addCodeAttribute() {
         'row-2': ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash'],
         'row-3': ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
         'row-4': ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight'],
-        'row-5': ['ControlLeft', 'MetaLeft', 'AltRight', 'Space', 'AltRight', 'Fn', 'LED', 'ControlRight'],
+        'row-5': ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'Fn', 'LED', 'ControlRight'],
     };
 
     let rowKyesIndex = 0; // индекс ряда в коллекции рядов-узлов клавиатуры
@@ -30,7 +31,35 @@ function addCodeAttribute() {
     }
 }
 
-document.addEventListener('keydown', (e) => {
+document.addEventListener('keydown', (event) => {
+    // Получили текущую нажатую клавишу из DOM 
+    const currentKeyDown = document.querySelector(`.keyboard .keyboard__key[data-keyCode="${event.code}"]`);
 
-    console.log(e);
+    // console.log(event);
+    // Если мы поймали CapsLock, то мы через toggle даём клавише активность
+    // При следующем нажатии на Caps, класс активности снимется и клавиша уже сама будет неактивна на аппаратном уровне
+    if (checkCapsLock(event)) {
+        currentKeyDown.classList.toggle('key-active');
+        return;
+    }
+    
+    // Если у нас не CapsLock, тогда мы добавляем активность на нажатую клавишу
+    addActiveClass(event, currentKeyDown);
+
+    // И при отпускании последней нажатой клавиши мы удаляем с неё класс активности
+    document.addEventListener('keyup', (event) => {
+        const currentKeyUp = document.querySelector(`.keyboard .keyboard__key[data-keyCode="${event.code}"]`);
+        // Если отпущена любая клавиша кроме пробела, то удаляем активность, а пробел остаётся активным до следующего нажатия
+        if (!checkCapsLock(event)) {
+            currentKeyUp.classList.remove('key-active');
+        }
+    });
 });
+
+function addActiveClass(event, currentKey) {
+    currentKey.classList.add('key-active');
+}
+
+function checkCapsLock(event) {
+    return (event.code == 'CapsLock') ? true : false;
+}
